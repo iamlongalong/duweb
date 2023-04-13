7909eb8faf037aafe59d676e5f7e9bc72cf6df8e
long <huhailong@jwzg.com>
2023-04-13 20:26:36 +0800

    add ugly sheet

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,45 +1,90 @@
 <template>
   <div class="hello">
     <div>
-      请输入要查询的目录: <input type="text" v-model="reqParams.PathName" placeholder="请输入要查询的目录">
+    服务器地址:
+      <input type="text" v-on:blur="changeURL" v-model="url" placeholder="请输入服务器地址">
     </div>
     <div>
-      请输入目录深度: <input type="text" v-model="reqParams.Deep" placeholder="请输入目录深度">
+    要查询的目录:
+      <input
+        type="text"
+        v-model="reqParams.PathName"
+        placeholder="请输入要查询的目录"
+      />
     </div>
     <div>
-      请输入每层最大数量: <input type="text" v-model="reqParams.MaxItems" placeholder="请输入每层最大数量">
+    目录深度:
+      <input
+        type="text"
+        v-model="reqParams.Deep"
+        placeholder="请输入目录深度"
+      />
     </div>
     <div>
-      请输入 others 的大小占比: <input type="text" v-model="reqParams.LongTailPercent" placeholder="请输入 others 的大小占比">
+    每层最大数量:
+      <input
+        type="text"
+        v-model="reqParams.MaxItems"
+        placeholder="请输入每层最大数量"
+      />
     </div>
     <div>
-      <input type="button" value="获取详细信息" @click="getDetail">
+    主体比例:
+      <input
+        type="text"
+        v-model="reqParams.LongTailPercent"
+        placeholder="请输入 others 的大小占比"
+      />
     </div>
+    <br>
+    <div>
+      <input type="button" value="获取详细信息" @click="getDetail" />
+      <input type="button" value="展示 treemap" @click="getTreeMapData" />
+    </div>
+    <div>
+      <!-- {{ treeMapData }} -->
+    </div>
+    <div id="diskinfo_plotly_block"></div>
   </div>
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from 'vuex';
+import { mapGetters, mapState, mapMutations, mapActions } from "vuex";
+import plotly from "plotly.js-dist";
 
 export default {
-  name: 'HelloWorld',
+  name: "HelloWorld",
   data() {
     return {
       reqParams: {
         PathName: "./",
         Deep: 1,
         MaxItems: 5,
-        LongTailPercent: 0.95
-      }
+        LongTailPercent: 0.95,
+      },
+      url: ""
     };
   },
+  computed: {
+    ...mapGetters(["treeMapData"]),
+  },
   methods: {
-    ...mapActions(['getDetails']),
+    ...mapActions(["getDetails"]),
+    ...mapMutations(["setUrl"]),
     getDetail() {
-      console.log(this);
-      this.getDetails(this.reqParams);
-    }
-  }
+      // console.log(this);
+      this.getDetails(this.reqParams);      
+    },
+    changeURL(e) {
+      // console.log("xxx : ", this.url)
+      this.setUrl(this.url)
+    },
+    getTreeMapData() {
+      // console.log("??????", this.treeMapData);
+
+      plotly.newPlot("diskinfo_plotly_block", this.treeMapData);
+    },
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -5,32 +5,85 @@ import querystring from "query-string";
 
 Vue.use(Vuex);
 
-let server_url = "http://127.0.0.1:8081";
+// let server_url = "http://127.0.0.1:8081";
+// let server_url = "http://172.17.5.170:8080";
 
 export default new Vuex.Store({
   state: {
     loading: false,
-    infos: [],
+    infos: {},
+    url: ""
   },
   mutations: {
     detailed(state, e) {
-      state.infos.push(e);
+      state.infos = e;
       console.log("get detailed mutation ", e);
-    }
+    },
+    setUrl(state, e) {
+      state.url = e
+    },
   },
   actions: {
-    getDetails({ state, commit }, e ) {
+    getDetails({ state, commit }, e) {
       let defaultParams = { PathName: ".", LongTailPercent: 0.95, Deep: 1, MaxItems: 8 };
-
       let query = querystring.stringify(Object.assign(defaultParams, e));
 
+      if (state.url == "") {
+        alert("must set url !!")
+        return
+      }
 
-      axios.get(`${server_url}/api/v1/info?${query}`).then(res => {
+      console.log("dfaldfjlas ", state.url)
+
+      axios.get(`${state.url}/api/v1/info?${query}`).then(res => {
         // commit('toggleLoading');
-        console.log("got info res ", res);
+        // console.log("got info res ", res);
         // state.images = res.data;
-        // commit('detailed', res.data);
+        commit('detailed', res.data);
       });
+    }
+  },
+  getters: {
+    treeMapData: state => {
+      let labels = []
+      let parents = []
+      let values = []
+
+      function walkPathInfo(node, parentName) {
+        labels.push(node.Name)
+        parents.push(parentName)
+        if (node.Childs.length > 0) {
+          values.push(0)
+        } else {
+          values.push(node.SizeKB)
+        }
+
+        if (!node.Childs) {
+          node.Childs = []
+        }
+        if (node.Childs.length > 0) {
+          node.Childs.forEach(el => {
+            walkPathInfo(el, node.Name)
+          });
+        }
+      }
+
+      // console.log(state.infos)
+      walkPathInfo(state.infos, "")
+
+      let data = [{
+        type: "treemap",
+        labels: labels,
+        parents: parents,
+        values: values,
+        textinfo: "label+value+percent parent+percent entry",
+        // domain: { "x": [0, 0.48] },
+        outsidetextfont: { "size": 20, "color": "#377eb8" },
+        marker: { "line": { "width": 1 } },
+        pathbar: { "visible": false }
+      }]
+      return data
     }
   }
 });
+
